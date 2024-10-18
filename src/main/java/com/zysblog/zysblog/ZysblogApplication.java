f150353c41de69631991ce7da9550792abf3ac74
heziyi <ziyihe@tencent.com>
2024-10-18 15:21:02 +0800

    first commit

---
+++ src/main/java/com/zysblog/zysblog/ZysblogApplication.java
@@ -0,0 +1,13 @@
+package com.zysblog.zysblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZysblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZysblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zysblog/zysblog/ZysblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zysblog.zysblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZysblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
